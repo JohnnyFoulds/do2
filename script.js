d53d9450b9e1fa71b9eadb062234318743dbfc77
Johannes Foulds <hfoulds@gmail.com>
2023-04-01 16:31:31 +0200

    Minor code clean

--- script.js
+++ script.js
@@ -6,19 +6,7 @@ $(document).ready(function () {
         const kg = 75;
         const do2 = co * (hb * 1.34 * sao2) * 10;
         $("#do2-val").text(do2.toFixed(2));
-        //$("#do2-slider").value(do2.toFixed(2));
     }
-
-    $("#do2-slider").slider({
-        orientation: "vertical",
-        min: 0,
-        max: 35000,
-        value: 5000,
-        slide: function (event, ui) {
-            //$("#do2-slider").text(ui.value);
-            //updateDO2();
-        },
-    });    
 
     $("#co-slider").slider({
         orientation: "vertical",
@@ -47,20 +35,8 @@ $(document).ready(function () {
         min: 0,
         max: 100,
         value: 98,
-        orientation: "vertical",
         slide: function (event, ui) {
             $("#sao2-val").text(ui.value);
-            updateDO2();
-        },
-    });
-
-    $("#pao2-slider").slider({
-        orientation: "vertical",
-        min: 40,
-        max: 150,
-        value: 100,
-        slide: function (event, ui) {
-            $("#pao2-val").text(ui.value);
             updateDO2();
         },
     });
